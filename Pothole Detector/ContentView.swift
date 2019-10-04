6631a313a255b2cf425e177bc9422ab41614608b
stevephan98 <stevephan98@github.com>
2019-10-04 10:29:02 -0400

    first commit

---
+++ Pothole Detector/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pothole Detector
+//
+//  Created by Steve Phan on 10/4/19.
+//  Copyright © 2019 Analog Dawgs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
