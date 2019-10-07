a5410da04ba0b243861aa647ae5a914e045024e6
stevephan98 <stevephan98@github.com>
2019-10-07 10:52:16 -0400

    first commit

--- Pothole Detector/ContentView.swift
+++ Pothole Detector/ContentView.swift
@@ -6,16 +6,14 @@
 //  Copyright © 2019 Analog Dawgs. All rights reserved.
 //
 
-import SwiftUI
+import UIKit
+import MapKit
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello World")
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+class MapScreen: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //mapView.delegate = self
     }
 }
